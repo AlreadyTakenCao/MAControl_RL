c9a398ce8601a376154da88da9896f6308ea87c5
wzq <you@you>
2019-10-07 11:34:18 +0800

    creation

--- MAControl/Util/Constrain.py
+++
@@ -1,7 +0,0 @@
-def constrain(x, Min, Max):
-    if x < Min:
-        return Min
-    elif x > Max:
-        return Max
-    else:
-        return x
--- README.md
+++
@@ -1,3 +0,0 @@
-# MAControl
-
-补充函数、临时变量请写在：MAControl.util。
